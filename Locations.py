13e02c4a3b9f228395ea11ada8de57694c0e9f9e
Fernando Martins <fernando.martins@iem.madeira.gov.pt>
2024-04-04 11:43:05 +0100

    Set location values

---
+++ Locations.py
@@ -0,0 +1,13 @@
+cities = [
+    {"name":"Calheta","id":"calheta"},
+    {"name":"Câmara de Lobos","id":"camralobos"},
+    {"name":"Funchal","id":"funchal"},
+    {"name":"Machico","id":"machico"},
+    {"name":"Ponta do Sol","id":"pontasol"},
+    {"name":"Porto Moniz","id":"portomoniz"},
+    {"name":"Porto Santo","id":"portosanto"},
+    {"name":"Ribeira Brava","id":"ribeirabrava"},
+    {"name":"Santa Cruz","id":"santacruz"},
+    {"name":"Santana","id":"santana"},
+    {"name":"São Vicente","id":"saovicente"},
+    ]
