93b51597de443abe46694b4270061615fb726924
Fernando Martins <fernando.martins@iem.madeira.gov.pt>
2024-04-04 14:22:06 +0100

    Improve code

---
+++ StatisticsData.py
@@ -0,0 +1,51 @@
+
+import Files
+import datetime
+import sys
+
+def countTotal(parts):
+    total = 0
+    for i in parts:
+        total+= i["text"] 
+
+    return total
+
+def prepareDataForSaving(year, month, data, parts, total, url):
+    if (year in data) is False :
+        data[year] = {}
+
+    data[year][month] = {"data":parts, "total":total, "url":url} 
+
+def main():
+    inputDate = None 
+    date = None
+
+    if len(sys.argv) > 1  : 
+        inputDate = sys.argv[1]
+        format_str = '%Y-%m-%d'  
+        try:
+            date = datetime.datetime.strptime(inputDate, format_str)         
+        except:
+            print("Invalid date format") 
+            return 1
+    else :
+        date = datetime.datetime.now()
+
+    filename = "metadata.pdf"
+
+    files = Files.Files()
+    url   = files.downloadFileFromUrl(date, filename)
+    parts = files.readPdfContent(filename)
+    total = countTotal(parts)
+    data  = files.getCurrentJsonDataFromFile("data.json")
+    year  = date.strftime("%Y")
+    month = date.strftime("%m")
+
+    prepareDataForSaving(year, month, data, parts, total, url)
+
+    files.setJsonToFile(filename, data)
+  
+    return 0  
+
+if __name__ == '__main__':
+    sys.exit(main()) 
